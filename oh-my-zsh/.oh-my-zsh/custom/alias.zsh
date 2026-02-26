71bc03b0fee1c697547757a2b73da3c2b6cf2207
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2026-02-26 16:31:18 -0300

    use new scripts to open new sessions

--- oh-my-zsh/.oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/alias.zsh
@@ -1,7 +1,7 @@
 alias dr="doppler run"
 alias drbe="doppler run -- bundle exec"
 alias drnr="doppler run -- npm run"
-alias ,cs=claude-session
-alias ,ds=destroy-session
+alias ,cs=claude-container-session
+alias ,ds=destroy-container-session
 alias mux=tmuxinator
 alias ts=tmux-sessionizer
